aed66a1e987f58ce564fd9be1bcc8aabfaebd0dd
Roman Chkhatarashvili <sadnewsforyou@gmail.com>
2019-03-18 16:26:51 +0300

    Practice 4.2.1

---
+++ Practice 4.2.1/js/script.js
@@ -0,0 +1,9 @@
+let number = prompt();
+number = parseFloat(number);
+if (number >= 0) {
+    console.log(number)
+} else if (number < 0){
+    console.log(-number);
+} else {
+	console.log(number);
+}
